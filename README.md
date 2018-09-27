3c700389add91e278a4359fda962b67506150f73
Sergio Martin <econsergio@gmail.com>
2018-09-27 23:02:36 +0100

    implement is video saved and update readme file

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # movies-monorepo
+## Movie Service API
+- GET /movies-api/movie_name
+- GET /movies-api/movie/movie_id
+## Video Service API
+- GET /video-api/movie_id?title=movie_name
+## Redis
+### Installation
+```sh
+sudo apt update             
+sudo apt install redis-server
+
+```
+### Console
+```sh
+redis-cli 
+```
+### Logs
+```sh
+redis-cli monitor
+```
+### Restart service
+```sh
+sudo systemctl restart redis
+```
--- video-service/src/main/java/org/smartinrub/videoservice/repository/VideoRepositoryImpl.java
+++ video-service/src/main/java/org/smartinrub/videoservice/repository/VideoRepositoryImpl.java
@@ -1,5 +1,6 @@
 package org.smartinrub.videoservice.repository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Slf4j
 @Repository
 public class VideoRepositoryImpl implements VideoRepository {
 
@@ -18,18 +20,22 @@ public class VideoRepositoryImpl implements VideoRepository {
     }
 
     @Override
-    public void saveLinks(String movideId, List<String> links) {
+    public void saveLinks(String movieId, List<String> links) {
+        log.info("Saving links of movie {} in Redis...", movieId);
         ListOperations<String, String> stringStringListOperations = redisTemplate.opsForList();
-        stringStringListOperations.leftPushAll(movideId, links);
+        stringStringListOperations.leftPushAll(movieId, links);
     }
 
     @Override
     public boolean isVideoSaved(String movieId) {
-        return false;
+        return redisTemplate.hasKey(movieId);
     }
 
     @Override
     public List<String> getLinks(String movieId) {
-        return null;
+        log.info("Retrieving links of movie {} in Redis...", movieId);
+        ListOperations<String, String> stringStringListOperations = redisTemplate.opsForList();
+        Long size = stringStringListOperations.size(movieId);
+        return stringStringListOperations.range(movieId, 0, size);
     }
 }
